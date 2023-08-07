92b43cc74d48dbb926d246f7a3ceb4323d185efd
greendrogon <farzanehrezaei28@yahoo.com>
2023-08-07 18:50:19 +0330

    submit/income & submit?expense:

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Expense 
-from .models import Income
+from .models import Income 
+from .models import Token
 # Register your models here.
 
 
 admin.site.register(Expense)
 admin.site.register(Income)
+admin.site.register(Token)
--- web/models.py
+++ web/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Token (models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return "{}-token".format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField() 
@@ -15,8 +21,9 @@ class Expense(models.Model):
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.DateTimeField() 
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True,)
-    def __srt__(self):
-        return "{}-{}".format(self.date,self.amount)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True,)  # har user be yek Expense vasl mishe
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount) 
+    
